016ba89ade78469c71dd6b13a9853ebb25b9205c
Ayselquliyeva <92804962+Ayselquliyeva@users.noreply.github.com>
2021-11-30 20:06:12 +0400

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,10 @@
+function yoxla(){
+let email="admin@gmail.com";
+let pass="123456";
+if(email=="admin@gmail.com" && pass=="123456"){
+    document.getElementById("test").innerHTML="siz sayta daxil oldunuz"
+}
+}
+  
+   
+    
